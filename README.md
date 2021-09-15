667da1ee957f26787430db81a2b13b03b99d395c
Nemeth Miklos <nemethmiklos1960@gmail.com>
2021-09-15 23:12:12 +0200

    Part 1 completed and working

---
+++ README.md
@@ -0,0 +1,20 @@
+# anna11
+Anna 11 Danesi
+
+[LitElement basics tutorial with TypeScript, 2020 video](https://youtu.be/1jn-NR4jiPk)
+[Lit Basics on Vaadin Fusion website](https://vaadin.com/docs/latest/fusion/application/lit) uses the most uptodate version
+
+Project was initiated with [ViteJS](https://vitejs.dev/)
+Select *vanilla-ts* template; don't select lit-element, since it creates a library project that cannot be deployed on Azure.
+```
+npm init vite
+Project name: anna11
+Select a framework: vanilla
+Select a variant: vanilla-ts
+```
+- npm install
+- npm i -D eslint
+- npx eslint --init
+- npm i lit
+- npm @vaadin/vaadin-text-field @vaadin/vaadin-button @vaadin/vaadin-ordered-layout @vaadin/vaadin-grid @vaadin/vaadin-form-layout
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,57 @@
-import './style.css'
+import "./style.css"
+import { LitElement,html, render, TemplateResult} from "lit"
+import { customElement, property, state  } from "lit/decorators.js"
+import "@vaadin/vaadin-text-field"
+import "@vaadin/vaadin-button"
+import "@vaadin/vaadin-ordered-layout/vaadin-vertical-layout"
+import "@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout"
+import "@vaadin/vaadin-grid/vaadin-grid"
+import "@vaadin/vaadin-form-layout/vaadin-form-layout"
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const app = document.querySelector<HTMLDivElement>("#app")!
+// app.innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+render(html`
+  <demo-view></demo-view>
+`, app)
+//From [LitElement basics tutorial with TypeScript (Vaadin Fusion client-side views), 2020 video](https://youtu.be/1jn-NR4jiPk)
+@customElement("demo-view")
+class DemoView extends LitElement {
+  @property() name = "Anna"
+  @state() people = [{name:"Juli"}]
+  override render():TemplateResult {
+    return html`
+    <vaadin-vertical-layout>
+      <h1>Hello ${this.name}</h1>
+      <vaadin-form-layout>
+        <vaadin-text-field label="Name" value=${this.name} @input=${this.onNameChange}></vaadin-text-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-end;">
+        <vaadin-button theme="primary" @click=${this.onAddButtonClick}>Add</vaadin-button>
+        <!-- vaadin-button theme="secondary">Cancel</vaadin-button -->
+      </vaadin-horizontal-layout>
+      <h1>People</h1> 
+      <!-- ul>
+        ${this.people.map(p=>html`
+          <li>${p.name}</li>
+        `)}
+      </ul -->
+      <vaadin-grid .items=${this.people}>
+        <!-- ${Object.keys(this.people[0])[0]} -->
+        <vaadin-grid-column text-align="start" path="name" header="Name"></vaadin-grid-column>
+      </vaadin-grid>
+    </vaadin-vertical-layout>
+    `
+  }
+  //No need to bind the method name when added as event listener, lit-html does behind the scene
+  onNameChange(e:Event):void {
+    this.name = (e.target as HTMLInputElement).value
+  } 
+  onAddButtonClick():void {
+    this.people = [...this.people,{name:this.name}]
+    this.name = ""
+  }
+}
+declare global { interface HTMLElementTagNameMap { "demo-view": DemoView}}
